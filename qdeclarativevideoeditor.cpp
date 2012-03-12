6ad8b7b6ae59d566c097f0bcee6335c4b2a8335c
Robert Swain <robert.swain@collabora.co.uk>
2012-03-12 10:12:14 +0100

    License: Add LGPL v2.1 license file and headers

--- qdeclarativevideoeditor.cpp
+++ qdeclarativevideoeditor.cpp
@@ -1,3 +1,25 @@
+/* VideoEditor
+ * Copyright (C) 2012 Thiago Sousa Santos <thiago.sousa.santos@collabora.co.uk>
+ * Copyright (C) 2012 Robert Swain <robert.swain@collabora.co.uk>
+ *
+ * gstpad.c: Pads for linking elements together
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include "qdeclarativevideoeditor.h"
 
 #include <QDebug>
